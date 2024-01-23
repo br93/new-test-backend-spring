6c1eb79aa2b02dac8eeec91a78a30e50f97b9535
br93 <reisalvesb@gmail.com>
2024-01-23 18:24:39 -0300

    Initial commit

---
+++ src/test/java/com/br93/testbackend/TestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br93.testbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
